7af5808d6660dd9fd80d06097908d592f5fd2bb5
cherkavskadasha <148155939+cherkavskadasha@users.noreply.github.com>
2025-02-02 01:07:16 +0200

    Update AddressesController.cs

--- AnimeShop/Controllers/AddressesController.cs
+++ AnimeShop/Controllers/AddressesController.cs
@@ -9,137 +9,128 @@ using AnimeShop.Models;
 
 namespace AnimeShop.Controllers
 {
-    public class AddressesController : Controller
+   public class AddressesController : Controller
+{
+    private readonly AnimeShopContext _context;
+
+    public AddressesController(AnimeShopContext context)
     {
-        private readonly AnimeShopContext _context;
-
-        public AddressesController(AnimeShopContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IActionResult> Index()
-        {
-            return View(await _context.Addresses.ToListAsync());
-        }
-
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var address = await _context.Addresses
-                .FirstOrDefaultAsync(m => m.AddressId == id);
-            if (address == null)
-            {
-                return NotFound();
-            }
-
-            return View(address);
-        }
-
-        // GET: Addresses/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("AddressId,Street,ZipCode")] Address address)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(address);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(address);
-        }
-
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var address = await _context.Addresses.FindAsync(id);
-            if (address == null)
-            {
-                return NotFound();
-            }
-            return View(address);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("AddressId,Street,ZipCode")] Address address)
-        {
-            if (id != address.AddressId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(address);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!AddressExists(address.AddressId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction("Dashboard", "Customer");
-            }
-            return View(address);
-        }
-
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var address = await _context.Addresses
-                .FirstOrDefaultAsync(m => m.AddressId == id);
-            if (address == null)
-            {
-                return NotFound();
-            }
-
-            return View(address);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var address = await _context.Addresses.FindAsync(id);
-            if (address != null)
-            {
-                _context.Addresses.Remove(address);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool AddressExists(int id)
-        {
-            return _context.Addresses.Any(e => e.AddressId == id);
-        }
+        _context = context;
     }
+
+    public async Task<IActionResult> Index()
+    {
+        return View(await _context.Addresses.ToListAsync());
+    }
+
+    public async Task<IActionResult> Details(int? id)
+    {
+        var address = await GetAddressByIdAsync(id);
+        if (address == null)
+        {
+            return NotFound();
+        }
+
+        return View(address);
+    }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind(nameof(Address.AddressId), nameof(Address.Street), nameof(Address.ZipCode))] Address address)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(address);
+        }
+
+        _context.Add(address);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    public async Task<IActionResult> Edit(int? id)
+    {
+        var address = await GetAddressByIdAsync(id);
+        if (address == null)
+        {
+            return NotFound();
+        }
+
+        return View(address);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind(nameof(Address.AddressId), nameof(Address.Street), nameof(Address.ZipCode))] Address address)
+    {
+        if (id != address.AddressId)
+        {
+            return NotFound();
+        }
+
+        if (!ModelState.IsValid)
+        {
+            return View(address);
+        }
+
+        try
+        {
+            _context.Update(address);
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!AddressExists(address.AddressId))
+            {
+                return NotFound();
+            }
+            throw;
+        }
+
+        return RedirectToAction(nameof(Index)); // Removed hardcoded redirect
+    }
+
+    public async Task<IActionResult> Delete(int? id)
+    {
+        var address = await GetAddressByIdAsync(id);
+        if (address == null)
+        {
+            return NotFound();
+        }
+
+        return View(address);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var address = await _context.Addresses.FindAsync(id);
+        if (address == null)
+        {
+            return NotFound();
+        }
+
+        _context.Addresses.Remove(address);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+    }
+
+    private bool AddressExists(int id) => _context.Addresses.Any(e => e.AddressId == id);
+
+    private async Task<Address?> GetAddressByIdAsync(int? id)
+    {
+        if (id == null)
+        {
+            return null;
+        }
+
+        return await _context.Addresses.FirstOrDefaultAsync(m => m.AddressId == id);
+    }
+}
 }
